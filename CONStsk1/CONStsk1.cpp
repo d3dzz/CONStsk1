17c04bf6f570b198c1c0944e68195af7c8555986
d3dzz <70903263+d3dzz@users.noreply.github.com>
2020-09-16 22:00:27 +0300

    Добавьте файлы проекта.

---
+++ CONStsk1/CONStsk1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	if ((a > b) && (b > c)) {
+		cout << c << ' ' << b << ' ' << a << '\n';
+	}
+	if ((b > a) && (c > b)) {
+		cout << a << ' ' << b << ' ' << c <<'\n';
+	}
+	if ((a < b) && (b > c) && (a < c)) {
+		cout << a << ' ' << c << ' ' << b << '\n';
+	}
+	if ((a > b) && (c > b) && (c > a)) {
+		cout << b << ' ' << a << ' ' << c << '\n';
+	}
+	if ((b > a) && (b > c) && (a > c)) {
+		cout << c << ' ' << a << ' ' << b << '\n';
+	}
+	if ((a > b) && (a > c) && (c > b)) {
+		cout << b << ' ' << c << ' ' << a << '\n';
+	} 
+	system("pause");
+	return 0;
+	
+	
+
+}
